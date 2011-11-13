d1e62dba8050ace6e7c0825c56d5d9350ad97f86
Jess Hester <jess1691@gmail.com>
2011-11-13 10:28:47 +0000

    Initial fields work; not mixins
    git-svn-id: https://svn.cs.hmc.edu/cs181b/fall11/ours/hsaxberg-jhester@795 b3dceee4-e711-448b-9f10-89700535f856

--- Project Source/fields.py
+++ Project Source/fields.py
@@ -3,41 +3,212 @@ Created on Nov 10, 2011
 
 @author: Haak Saxberg and Jess Hester
 '''
+import warnings
+import datetime
+import re
+
+from decorators import OProperty
 
 class FieldDNE(Exception):
     pass
 
+class ValidationError(ValueError):
+	pass
+
 class Field(object):
-    creation_counter = 0
+    """Abstract base class for universal field objects (UFOs)
+        
+    Keyword arguments: 
+    kwargs -- as yet undetermined
     
-    default_error_messages = {
-                              'null':_(u"This field cannot be null"),
-                              'blank':_(u"This field cannot be blank"),
-                              'unique':_(u"{model} with this {field} already exists"),
-                              }
-    
-    def _description(self):
-        return u"Field <{type}>".format(**{"type":self.__class__.__name__})
-    
-    description = property(_description)
-    
-    def __init__(self, **kwargs):
-        pass
-    
-    def __cmp__(self, rhs):
-        pass
-    
-    def __deepcopy__(self, memodict):
-        pass
-    
+    """
+
     def to_python(self, value):
-        pass
-    
-    def db_type(self, connection):
-        pass
-    
-    @property
-    def unique(self):
-        return self._unique or self.primary_key
-    
-    
+        """Converts field objects from internal representation to Python data types"""
+        raise NotImplementedError("to_python not overriden")
+
+    def getvalue(self):
+        """Field value as a Python datatype. Calls to_python"""
+        raise NotImplementedError("Value getter not overriden")
+
+    def setvalue(self, val):
+        """Sets the value; converts from a Python datatype to an internal representation"""
+        raise NotImplementedError("Value setter not overriden")
+
+    def delvalue(self):
+        """Deletes this field's value"""
+        raise warnings.warn("No way to delete field values",Warning)
+	value = OProperty(getvalue, setvalue, delvalue, "value of this field")
+
+    def validate(self, val):
+        """Ensures Python datatype/internal representation translation is valid"""
+       	for validator in self._validators:
+			validator(value)
+
+		return True
+
+    def clean(self):
+        """Sanitizes the data before carrying out operations"""
+        raise NotImplementedError("Sanitation not implemented")
+        
+    def translate(self):
+        """The actual translation from our Python representation to backend-readable representation"""
+        raise NotImplementedError("Translation not implemented")
+
+	def __init__(self, *args, **kwargs):
+		self._validators = ()
+
+class StringField(Field):
+    """ """
+	def to_python(self, value):
+        """Converts from intermediate StringField representation to Python string"""
+	    if value is None: 
+			return value
+		return unicode(value)
+
+	def setvalue(self, val):
+	    """Takes a string and stores it as a StringField"""
+		self._value = val if self.validate(val)
+
+	def getvalue(self):
+		"""Return the string representation of the StringField"""
+		self.to_python(self, self._value)
+
+	def delvalue(self):
+		"""Sets value of StringField to None"""
+		self._value = None
+
+	def validate(self, val):
+		"""Ensures value is a properly formatted string"""
+		#TODO: add keyword activated validators
+		try:
+			self._value = str(self._value)
+		except Exception, e:
+			raise ValidationError(r"Could not convert to string: {}".format(self._value))
+		
+		return super(StringField, self).validate(val)
+
+class IntegerField(Field):
+	"""Class for Intergers"""
+	def to_python(self, value):
+		if value is None: 
+			return value
+		return int(value)
+
+	def setvalue(self, val):
+		"""Takes an int and stores it as an IntegerField"""
+		self._value = val if self.validate(val)
+
+	def getvalue(self):
+		self.to_python(self, self._value)
+
+	def delvalue(self):
+		self._value=None
+
+	def validate(self, val):
+		try:
+			self._value = int(self._value)
+		except Exception, e:
+			raise ValidationError(r"Could not convert to int: {}".format(self._value))
+		
+		return super(IntegerField, self).validate(val)
+
+class FloatField(Field):
+	def to_python(self, value):
+		if value is None:
+			return value
+		return float(value)
+
+	def setvalue(self, val):
+		self._value = val if self.validate(val)
+
+	def getvalue(self):
+		self.to_python(self, self._value)
+
+	def delvalue(self):
+		self._value = None
+
+	def validate(self, val):
+		try:
+			self._value = float(self._value)
+		except Exception, e:
+			raise ValidationError(r"Could not convert to float: {}".format(self._value))
+		
+		return super(FloatField, self).validate(val)
+
+class BooleanField(Field):
+	def to_python(self, value):
+		if value is None:
+			return value
+		return boolean(value)
+
+	def setvalue(self, val):
+		self._value = val if self.validate(val)
+
+	def getvalue(self):
+		self.to_python(self, self._value)
+
+	def delvalue(self):
+		self._value = None
+
+	def validate(self, val):
+		try:
+			value = boolean(val)
+		except Exception, e:
+			raise ValidationError(r"Could not convert to boolean: {}".format(self._value))
+		
+		return super(BooleanField, self).validate(val)
+		
+
+class DateField(Field):
+	ansi_date_re = re.compile(r'^\d{1,2}-\d{1,2}-\d{4}$')
+
+	def to_python(self, value):
+        if value is None:
+            return value
+        if isinstance(value, datetime.datetime):
+            return value.date()
+        if isinstance(value, datetime.date):
+            return value
+		if isinstance(value, int):
+			return datetime.datetime.utcfromtimestamp(value).date()
+		try:
+			return datetime.datetime.strptime(val, "%d/%m/%Y").date()
+		except:
+			# Now that we have the date string in YYYY-MM-DD format, check to make
+			# sure it's a valid date.
+			# We could use time.strptime here and catch errors, but datetime.date
+			# produces much friendlier error messages.
+			day, month, year = map(int, value.split('-'))
+			try:
+				return datetime.date(year, month, day)
+			except ValueError, e:
+				msg = self.error_messages['invalid_date'] % _(str(e))
+				raise exceptions.ValidationError(msg)
+
+	def setvalue(self, val):
+		self._value = val if self.validate(val)
+		
+	def getvalue(self):
+		self.to_python(self, self._value)
+
+	def delvalue(self):
+		self._value = None
+
+	def validate(self, val):
+		if isinstance(str, val):
+			try:
+				datetime.datetime.strptime(val, "%d/%m/%Y").date()
+			except ValueError, e:
+				raise ValidationError(r"Couldn't coerce into a date: {}".format(val))
+		elif isinstance(int, val):
+			try:
+				datetime.datetime.utcfromtimestamp(val).date()
+			except ValueError, e:
+				raise ValidationError(r"Not a valid timestamp: {}".format(val))
+        elif not ansi_date_re.search(value):
+            raise exceptions.ValidationError(self.error_messages['invalid'])
+		else:
+			raise ValidationError(r"Could not create a date object: {}".format(val))
+
+		return super(DateField, self).validate(val)
--- Project Source/models.py
+++ Project Source/models.py
@@ -1,88 +1,55 @@
 '''
 Created on Nov 10, 2011
 
-@author: haak
+@author: Haak Saxberg, Jess Hester
 '''
 import abc
 
 from model_exceptions import *
 
-class BaseModel(type):
-    ### All models will inherit fromt his
-    
-    def __new__(cls, name, bases, attrs):
-        super_new = super(ModelBase, cls).__new__
-        parents = [b for b in bases if isinstance(b, ModelBase)]
-        if not parents:
-            # not a model, nothing special:
-            return super_new(cls, name, bases, attrs)
-        
-        #create class
-        module = attrs.pop('__module__')
-        new_class = super_new(cls, name, bases, module)
-    
-        attr_meta = attrs.pop('Meta', None)
-        abstract = getattr(attr_meta, 'abstract', False)
-        if not attr_meta:
-            meta = getattr(new_class, 'Meta', None)
-        else:
-            meta = attr_meta
-        base_meta = getattr(new_class, "_meta", None)
-    
-        new_class.add_to_class('_meta', meta)
-        if not abstract:
-            new_class.add_to_class('DNE', subclass_exception('DNE',
-                                                             tuple(x.DoesNotExist
-                                                                        for x in parents if hasattr(x, '_meta') and not x._meta.abstract)
-                                                             or (ObjectDoesNotExist,), module))
-            new_class.add_to_class('MultipleReturns', subclass_exception('MultipleObjectsReturned',
-                                                             tuple(x.MultipleObjectsReturned
-                                                                        for x in parents if hasattr(x, '_meta') and not x._meta.abstract)
-                                                             or (MultipleObjectsReturned,), module))
-            if base_meta and not base_meta.abstract:
-                if not hasattr(meta, 'ordering'):
-                    new_class._meta.ordering = base_meta.ordering
-                if not hasattr(meta, 'get_latest_by'):
-                    new_class._meta.get_latest_by = base_meta.get_latest_by
-                    
-        if getattr(new_class, '_default_manager', None):
-            new_class._default_manager = new_class.default_manager._copy_to_model(new_class)
-            new_class._base_manager = new_class._base_manager._copy_to_model(new_class)
-            
-        m = get_model(new_class._meta.app_label, name)
-        if m is not None:
-            return m
-        
-    
-    def add_to_class(cls, name, value):
-        if hasattr(value, 'contribute_to_class'):
-            value.contribute_to_class(cls, name)
-        else:
-            setattr(cls, name, value)
-
-class ModelState(object):
-    def __init__(self, db=None):
-        self.db = db
-        self. adding = True
-
 class Model(object):
-    __metaclass__ = BaseModel
+	"""Abstract base class for universal model objects
 
-    def __init__(self, *args, **kwargs):
-        if not hasattr(kwargs, "primary_key"):
-            raise NoPrimaryKey
-        
-        self._state = ModelState()
+	Keyword arguments:
+	kwargs -- undetermined
 
-        if kwargs:
-            for key in kwargs:
-                if isinstance(getattr(self.__class__, prop), property):
-                    setattr(self, prop, kwargs.pop(prop))
+	"""
+	__metaclass__ = abc.ABCMeta
+	
+	@classmethod
+	def create(cls, **kwargs):
+		"""Creates an internal representation of a Model from kwargs"""
+		raise NotImplementedError(u"Create not overridden")
 
-        if kwargs:
-            raise TypeError("'{}' is an invalid kwarg for this function".format(kwargs.keys()[0]))
+	@classmethod
+	def get(cls, key):
+		"""Gets an instance of the model using a unique primary key"""
+		raise NotImplementedError(u"Get not overridden")
 
-        super(Model, self).__init__()
+	def get_interface(self):
+		"""Returns an interface to the correct backend API"""
+		raise NotImplementedError(u"Get interface not overridden")
 
-def subclass_exception(name, parents, module):
-    return type(name, parents, {'__module__': module})
+	def put(self):
+		"""Store this instance in the backend"""
+		raise NotImplementedError(u"Put not overridden")
+
+	def pk(self):
+		"""Gets the primary key field's value for this model"""
+		pks = []
+		for key, val in self.__class__.__dict__:
+			# only Fields can act as primary keys:
+			if not isinstance(value, Field):
+				continue
+			
+			if value._meta["pk"]:
+				pks += [val]
+		
+		# make sure there's only ONE primary key
+		if len(pks) > 1:
+			raise RuntimeException(u"Too many primary keys for {}".format(self))
+
+		return pks[0].value
+
+	def __repr__(self):
+		return u"QUI Model: {}".format(self.__name__)
